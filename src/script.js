fae366250965e53f2b38a87f72258148b99a8e6b
s0ru <apazgarcia23@outlook.es>
2024-05-19 01:14:16 +0200

    initial commit

---
+++ src/script.js
@@ -0,0 +1,111 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+// Debug
+const gui = new GUI()
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+const gltfLoader = new GLTFLoader()
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(7, 7, 7)
+scene.add(camera)
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+const rendererParameters = {}
+rendererParameters.clearColor = '#1d1f2a'
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setClearColor(rendererParameters.clearColor)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+gui
+    .addColor(rendererParameters, 'clearColor')
+    .onChange(() =>
+    {
+        renderer.setClearColor(rendererParameters.clearColor)
+    })
+
+
+const material = new THREE.MeshBasicMaterial()
+
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+    material
+)
+torusKnot.position.x = 3
+scene.add(torusKnot)
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    material
+)
+sphere.position.x = - 3
+scene.add(sphere)
+
+let suzanne = null
+gltfLoader.load(
+    './suzanne.glb',
+    (gltf) =>
+    {
+        suzanne = gltf.scene
+        suzanne.traverse((child) =>
+        {
+            if(child.isMesh)
+                child.material = material
+        })
+        scene.add(suzanne)
+    }
+)
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    if(suzanne)
+    {
+        suzanne.rotation.x = - elapsedTime * 0.1
+        suzanne.rotation.y = elapsedTime * 0.2
+    }
+
+    sphere.rotation.x = - elapsedTime * 0.1
+    sphere.rotation.y = elapsedTime * 0.2
+
+    torusKnot.rotation.x = - elapsedTime * 0.1
+    torusKnot.rotation.y = elapsedTime * 0.2
+
+    controls.update()
+
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
